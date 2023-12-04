dae1523555d838d66a0e3f6340f1a4dc75dd5ebc
Alexandr Varenyshev <alex.varenyshev@gmail.com>
2023-12-04 22:29:25 +0300

    tr

--- src/test/java/PdfParsingTest.java
+++ src/test/java/PdfParsingTest.java
@@ -1,2 +1,33 @@
-package PACKAGE_NAME;public class PdfParsingTest {
+import com.codeborne.pdftest.PDF;
+import com.codeborne.xlstest.XLS;
+import com.opencsv.CSVReader;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+
+import java.io.InputStreamReader;
+import java.util.List;
+
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
+
+import com.codeborne.pdftest.PDF;
+import org.junit.jupiter.api.Test;
+
+import java.io.FileInputStream;
+import java.io.InputStream;
+
+public class PdfParsingTest {
+    private ClassLoader cl = PdfParsingTest.class.getClassLoader();
+
+    @Test
+    void pdfParsingTest() throws Exception {
+        try (InputStream is = cl.getResourceAsStream("example.pdf")
+             PDF pdf=new PDF()
+//        InputStream stream = cl.getResourceAsStream("examplePDF.pdf");
+//        PDF pdf = new PDF();
+    }
 }
+
+//        InputStream stream = getClass().getResourceAsStream("examplePDF.pdf");

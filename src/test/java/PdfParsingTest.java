c9d2e35dbb90bd6720cd2650b738db9e5215bbdb
Alexandr Varenyshev <alex.varenyshev@gmail.com>
2023-12-06 22:15:31 +0300

    tryng install parsing

--- src/test/java/PdfParsingTest.java
+++ src/test/java/PdfParsingTest.java
@@ -4,15 +4,10 @@ import com.opencsv.CSVReader;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
-
 import java.io.InputStreamReader;
 import java.util.List;
-
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
-
-import com.codeborne.pdftest.PDF;
 import org.junit.jupiter.api.Test;
 
 import java.io.FileInputStream;
@@ -23,11 +18,11 @@ public class PdfParsingTest {
 
     @Test
     void pdfParsingTest() throws Exception {
-        try (InputStream is = cl.getResourceAsStream("example.pdf")
-             PDF pdf=new PDF()
-//        InputStream stream = cl.getResourceAsStream("examplePDF.pdf");
-//        PDF pdf = new PDF();
+        try (InputStream is = cl.getResourceAsStream("examplePDF.pdf");
+             PDF pdf = new PDF(is);
+             Assertions.assertEquals("Chromium",pdf.text());
     }
 }
-
+//        InputStream stream = cl.getResourceAsStream("examplePDF.pdf");
+//        PDF pdf = new PDF();
 //        InputStream stream = getClass().getResourceAsStream("examplePDF.pdf");
